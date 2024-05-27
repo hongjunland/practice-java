d249bade0c4379ea52135e5b1f5c1948ecfbe760
Hongjun Lee <zunza96@gmail.com>
2024-05-27 12:05:29 +0900

    chore: reconstruct package - 행동패턴(behavior) 패키지로 구분

--- src/test/java/designpattern/iteratorpattern/aggregate/Aggregate.java
+++
@@ -1,7 +0,0 @@
-package designpattern.iteratorpattern.aggregate;
-
-import designpattern.iteratorpattern.itrerator.Iterator;
-
-public interface Aggregate<T> {
-    Iterator<T> createIterator();
-}
--- src/test/java/designpattern/iteratorpattern/itrerator/Iterator.java
+++
@@ -1,6 +0,0 @@
-package designpattern.iteratorpattern.itrerator;
-
-public interface Iterator<T> {
-    boolean hasNext();
-    T next();
-}
